849abe2c5c24f043520a0773d6315db298d24d7e
Nir Swartz <62308517+nirswartz@users.noreply.github.com>
2020-08-06 18:37:21 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
-#include "spmat.h";
-#include <stdlib.h>;
+#include <stdio.h>
+#include "spmat.h"
 
-int main(){
-	int arr[3][3] = {{61,0,0},{0,0,0},{0,84,22}};
-	spmat *mysparse = spmat_allocate_array(3,3);
-	for (int i = 0; i < 3; ++i) {
-		add_row(mysparse, arr[i], i);
-	}
-	printSparse(mysparse);
 
-	return 0;
-
+int main() {
+    int i;
+    int arr[3][3] = {{61,0,0},{0,0,0},{0,84,22}};
+    spmat *mys = NULL;
+    mys=spmat_allocate_array(3,3);
+    for (i = 0; i < 3; ++i) {
+        mys->add_row(mys, arr[i], i);
+    }
+    mys->printSparse();
+    return 0;
 }
