53d2de64de0b0b91315ed3a648811c5edbee8abf
Sonia-Mo <69247492+Sonia-Mo@users.noreply.github.com>
2020-08-23 11:56:26 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "spmat.h"
 #include "modMat.h"
+#include "errors.h"
 
 int main() {
-	/* main for spmat!!
 
-    int i;
-    double arr[3][3] = {{61,0,0},{0,50,0},{0,84,22}};
+    int i, j;
+    double arr[3][3] = {{61,50,0},{0,0,0},{0,84,22}};
     double brr[3] = {1,-1,1};
     double res[3];
     spmat *mys = NULL;
@@ -16,6 +17,11 @@ int main() {
     for (i = 0; i < 3; ++i) {
         mys->add_row(mys, arr[i], i);
     }
+    for (i = 0; i < 3; ++i) {
+        for (j = 0; j < 3; ++j) {
+        	printf("A[%d,%d] equals : %f\n", i,j, mys->getA(mys,i,j));
+        }
+    }
 
     mys->mult(mys,brr,res);
     printf("{");
@@ -23,12 +29,24 @@ int main() {
            printf("%f,",*(res+i));
        }
        printf("}\n");
-    mys->printSparse(mys);*/
+    mys->printSparse(mys);
 
-	 FILE *f1;
-	 f1 = fopen("graph.in","r");
-	 modMat *myMat = modMat_allocate(f1);
+
+	/*FILE *f1;
+	 modMat *myMat;
+	 int n, *numOfNodes;
+
+	 printf("check!!1");
+
+
+	 f1 = fopen("src\\graph.in","r");
+	 n = fread(&numOfNodes, sizeof(int), 1, f1);
+	 printf("check!!1");
+	 checkItemsRead(n,1,__LINE__);
+	 printf("check!!1~");
+	 myMat = modMat_allocate(f1);
 	 printf("%d",myMat->n);
-
+	 printf("check");
+*/
     return 0;
 }
