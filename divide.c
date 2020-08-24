8a8bc8d4599d93c268a9def1d55193286e6acd55
Sonia-Mo <69247492+Sonia-Mo@users.noreply.github.com>
2020-08-24 18:17:53 +0300

    Add files via upload

--- divide.c
+++ divide.c
@@ -1,3 +1,56 @@
-/* to be continue*/
+#include "eigen.h"
+
+void createS(double *eigenvector, int *s, int vectorSize){
+	int i;
+	for (i = 0; i < vectorSize; ++i) {
+		if(*eigenvector > 0){
+			*s = 1;
+		}
+		else{
+			*s = -1;
+		}
+		eigenvector++;
+		s++;
+	}
+}
+
+double computeEigenvalue(modMat *B ,double *eigenvector){
+
+
+	return 0.0;
+}
+
+
+int* Algorithm2(modMat *B, int *arr, int *g){
+	double *eigenvector, lambda, Q;
+	int *s, division[2];
+
+	eigenvector = calloc(B->n, sizeof(double));
+    checkAllocation(eigenvector, __LINE__,__FILE__);
+
+    /* stage 1 - compute leading eigenvector */
+    powerIteration(B, eigenvector, g);
+    lambda = computeEigenvalue(B,eigenvector); /*ליצור פונקציה */
+
+    /* stage 2 */
+    if(lambda <= 0){
+    	/*return NULL;*/
+    }
+    /* stage 3 - compute s */
+    s = calloc(B->n, sizeof(int));
+    createS(eigenvector, s, B->n);
+
+    /* stage 4 - compute s^T*B_hat[g]*s */
+    Q = computeDeltaQ (B, s, g);
+    if (Q <= 0){
+    	/*return NULL;*/
+    }
+
+    maximization(B,s);
+
+    /* stage 5 - divide s into g_1 and g_2 */
+    makeDivision(division);
+    return division;
+}
 
 
