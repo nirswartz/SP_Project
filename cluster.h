82f68da0121ed63b0c65f697e556d375890f5e95
Nir Swartz <62308517+nirswartz@users.noreply.github.com>
2020-09-19 20:03:42 +0300

    Delete cluster.h

--- cluster.h
+++
@@ -1,6 +0,0 @@
-#ifndef SP_PROJECT_CLUSTER_H
-#define SP_PROJECT_CLUSTER_H
-/*int cluster(int argc, char* argv[]);*/
-int cluster(int argc, char* argv[]);
-#endif
-/*%%%%%%%%%%%% delete all file %%%%%%%%%%%%%%%%%%%%*/
